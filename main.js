4387838b0d323bc6d52f6fd6fb74edcbb84b595f
Raul Jimenez <rjbarco@gmail.com>
2022-06-13 11:25:55 +0200

    gameBoard and displayController modules created. 'Player' factory created. Two Player instances created

---
+++ main.js
@@ -0,0 +1,20 @@
+// A module for the game board
+let gameBoard = (function() {
+    'use strict';
+
+    let _board = [];
+})();
+
+// A module for the game flow
+let displayController = (function() {
+    'use strict';
+})();
+
+// A factory for the players
+const Player = () => {
+
+};
+
+//We need two players to play the game
+const player1 = Player();
+const player2 = Player();
